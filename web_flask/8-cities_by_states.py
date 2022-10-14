46bf6650c88266f95023fd839d897a688adde495
root <jedidiabahena@gmail.com>
2022-10-14 15:19:54 +0000

    fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
+"""script to start a flask app on localhost
 """
-starts a Flask web application
-"""
-
-from flask import Flask, render_template
-from models import *
 from models import storage
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def appcontext_teardown(exc=None):
+    """called on teardown of app contexts,
+        for more info on contexts visit
+        -> http://flask.pocoo.org/docs/1.0/appcontext/
+        Storage.close() closes the sql scoped session or reloads file
+            storage.
+    """
     storage.close()
 
+
+@app.route('/cities_by_states', strict_slashes=False)
+def conditional_templating(n=None):
+    """checking input data using templating"""
+    states = storage.all("State")
+    data = {}
+    return render_template('8-cities_by_states.html',
+                           states=storage.all("State"))
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
