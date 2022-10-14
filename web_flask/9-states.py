430d4582dc85710cdeefe86d7aeb456bfd6b5e2c
root <jedidiabahena@gmail.com>
2022-10-14 15:17:13 +0000

    fixed

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
+"""script to start a flask app on localhost
 """
-starts a Flask web application
-"""
-
-from flask import Flask, render_template
-from models import *
 from models import storage
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def appcontext_teardown(exc=None):
+    """called on teardown of app contexts,
+        for more info on contexts visit
+        -> http://flask.pocoo.org/docs/1.0/appcontext/
+        Storage.close() closes the sql scoped session or reloads file
+            storage.
+    """
     storage.close()
 
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def conditional_templating(id=None):
+    """checking input data using templating"""
+    states = storage.all("State")
+    if id is None:
+        return render_template('9-states.html',
+                               states=states)
+    state = states.get('State.' + id)
+    return render_template('9-states.html',
+                           state=state)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
